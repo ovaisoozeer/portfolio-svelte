1613f330f8b50ea145a13e31860a8745d89bd3cb
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-07 00:17:50 +0000

    Adds some placeholders for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,37 @@
-<h1>Home</h1>
+<h1 class="text-4xl">Hi, my name is Ovais 👋🏽</h1>
+<h4 class="text-xl">pronouns: he/him, pronounced: əʊveɪs</h4>
+
+<h2>Which of the following best describes the reason for your visit?</h2>
+
+<div class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-3">
+	<div class="card bg-base-200 shadow-xl">
+		<div class="card-body">
+			<h2 class="mb-4 card-title">
+				You're curious, and would like a simple introduction
+			</h2>
+			<div class="card-actions justify-end">
+				<button type="button" class="btn btn-primary">Get to know me</button>
+			</div>
+		</div>
+	</div>
+	<div class="card bg-base-200 shadow-xl">
+		<div class="card-body">
+			<h2 class="mb-4 card-title">
+				You know me, and are here for a purpose
+			</h2>
+			<div class="card-actions justify-end">
+				<button type="button" class="btn btn-primary">Get straight to brass tacks</button>
+			</div>
+		</div>
+	</div>
+	<div class="card bg-base-200 shadow-xl">
+		<div class="card-body">
+			<h2 class="mb-4 card-title">
+				You have an open mind, and want to see where this might go
+			</h2>
+			<div class="card-actions justify-end">
+				<button type="button" class="btn btn-primary">Get weird</button>
+			</div>
+		</div>
+	</div>
+</div>
