2ae6b7f7fbdfbf0fe255132e96ca192754c321f7
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-08 14:52:55 +0000

    Minor: code formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,27 @@
 <div class="flex w-full flex-col items-center">
-	<h1 class="m-4 text-center text-6xl">Hi, my name is Ovais 👋🏽</h1>
-	<h4 class="text-l mt-4 mb-8 text-center"><em>(əʊveɪs, he/him)</em></h4>
+  <h1 class="m-4 text-center text-6xl">Hi, my name is Ovais 👋🏽</h1>
+  <h4 class="text-l mt-4 mb-8 text-center"><em>(əʊveɪs, he/him)</em></h4>
 </div>
 <div class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-3">
-	<div class="card bg-base-200 shadow-xl">
-		<div class="card-body">
-			<h2 class="m-4 card-title justify-center">Start here</h2>
-			<div class="m-4 card-actions justify-center">
-				<button type="button" class="btn btn-primary">Portfolio</button>
-			</div>
-		</div>
-	</div>
-	<div class="card bg-base-200 shadow-xl">
-		<div class="card-body">
-			<h2 class="m-4 card-title justify-center">Work with me</h2>
-			<div class="m-4 card-actions justify-center">
-				<button type="button" class="btn btn-primary">Brass tacks</button>
-			</div>
-		</div>
-	</div>
-	<div class="card bg-base-200 shadow-xl">
-		<div class="card-body">
-			<h2 class="m-4 card-title justify-center">Explore what I do</h2>
-			<div class="m-4 card-actions justify-center">
-				<button type="button" class="btn btn-primary">Content, Community, and Events</button>
-			</div>
-		</div>
-	</div>
+  <a href="about">
+    <div class="card bg-base-200 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title justify-center">Portfolio</h2>
+      </div>
+    </div>
+  </a>
+  <a href="brass-tacks">
+    <div class="card bg-base-200 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title justify-center">Work with me</h2>
+      </div>
+    </div>
+  </a>
+  <a href="brass-tacks">
+    <div class="card bg-base-200 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title justify-center text-center">Content, Community, Events</h2>
+      </div>
+    </div>
+  </a>
 </div>
