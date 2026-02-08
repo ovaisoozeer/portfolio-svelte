24790460f7f0b5c4c2447f6c6eb01b4ed87d0d99
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-08 23:04:22 +0000

    Home page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,28 @@
-<div class="mb-8 flex w-full flex-col items-center">
+<div class="mb-24 flex w-full flex-col items-center">
   <h1 class="m-4 text-center text-6xl tracking-wide">Hi, my name is Ovais</h1>
-  <h4 class="text-l mt-4 mb-8 text-center tracking-wide"><em>(əʊveɪs, he/him)</em></h4>
+  <h4 class="text-l mb-8 text-center tracking-wide"><em>(əʊveɪs, he/him)</em></h4>
 </div>
-<div class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-3">
+<div class="mt-8 mb-24 grid grid-cols-1 gap-6 md:grid-cols-3">
   <a href="about">
-    <div class="card bg-base-200 shadow-xl">
+    <div class="card bg-base-200 shadow-xl p-8">
       <div class="card-body">
-        <h2 class="card-title justify-center text-center tracking-wide">Get to know me a little</h2>
+        <h2 class="card-title justify-center text-center tracking-wide">About</h2>
       </div>
     </div>
   </a>
   <a href="explore">
-    <div class="card bg-base-200 shadow-xl">
+    <div class="card bg-base-200 shadow-xl p-8">
       <div class="card-body">
         <h2 class="card-title justify-center text-center tracking-wide">
-          Explore content, community, and events
+          Content, Community, and Events
         </h2>
       </div>
     </div>
   </a>
   <a href="brass-tacks">
-    <div class="card bg-base-200 shadow-xl">
+    <div class="card bg-base-200 shadow-xl p-8">
       <div class="card-body">
-        <h2 class="card-title justify-center text-center tracking-wide">Get in touch</h2>
+        <h2 class="card-title justify-center text-center tracking-wide">Brass tacks</h2>
       </div>
     </div>
   </a>
