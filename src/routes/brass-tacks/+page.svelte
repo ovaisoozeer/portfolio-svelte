9c60c2ec085ad0624266f39d31f7a28f8528f03f
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-11 12:11:00 +0000

    Adds basic brass tacks page

--- src/routes/brass-tacks/+page.svelte
+++ src/routes/brass-tacks/+page.svelte
@@ -1,8 +1,14 @@
+<script>
+  import cvpdf from '$lib/assets/cv.pdf';
+</script>
+
 <div class="mb-8 flex w-full flex-col">
   <h2 class="m-4 text-center text-4xl tracking-wide">Brass tacks</h2>
-  <p class="text-l mb-4 text-left tracking-wide">Download CV (TODO CV link)</p>
-  <p class="text-l mb-4 text-left tracking-wide">Explore services (TODO services link)</p>
-  <p class="text-l mb-4 text-left tracking-wide">Book a meeting (TODO gcal link)</p>
-  <p class="text-l mb-4 text-left tracking-wide">Email (TODO Link)</p>
-  <p class="text-l mb-4 text-left tracking-wide">Telephone (TODO Link)</p>
+  <p class="text-l mb-4 text-left tracking-wide">
+    <a download="ovais-oozeer-cv.pdf" href={cvpdf} class="link">Download CV</a>
+  </p>
+  <p class="text-l mb-4 text-left tracking-wide"><a href="services" class="link">Explore services</a></p>
+  <p class="text-l mb-4 text-left tracking-wide"><a href="mailto:ovais@expertlypragmatic.com" class="link">Send an email to ovais@expertlypragmatic.com</a></p>
+  <p class="text-l mb-4 text-left tracking-wide"><a href="tel:+443300430764">Call me on +44 (0)330 0430764</a></p>
+  <p class="text-l mb-4 text-left tracking-wide"><a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9" class="link">Book a meeting (opens gCal in new tab)</a></p>
 </div>
