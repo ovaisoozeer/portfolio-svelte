d02a1139971276838da102113468f5cb9cdd5200
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-11 16:53:07 +0000

    Updates brass tacks

--- src/routes/brass-tacks/+page.svelte
+++ src/routes/brass-tacks/+page.svelte
@@ -1,14 +1,61 @@
-<script>
+<script lang="ts">
   import cvpdf from '$lib/assets/cv.pdf';
+  import { Section, HeroHeader, FeatureDefault, FeatureItem } from 'flowbite-svelte-blocks';
+  import {
+    ChartPieSolid,
+    LandmarkSolid,
+    BriefcaseSolid,
+    DollarOutline,
+    RocketSolid,
+    CogOutline
+  } from 'flowbite-svelte-icons';
 </script>
 
-<div class="mb-8 flex w-full flex-col">
-  <h2 class="m-4 text-center text-4xl tracking-wide">Brass tacks</h2>
-  <p class="text-l mb-4 text-left tracking-wide">
-    <a download="ovais-oozeer-cv.pdf" href={cvpdf} class="link">Download CV</a>
-  </p>
-  <p class="text-l mb-4 text-left tracking-wide"><a href="services" class="link">Explore services</a></p>
-  <p class="text-l mb-4 text-left tracking-wide"><a href="mailto:ovais@expertlypragmatic.com" class="link">Send an email to ovais@expertlypragmatic.com</a></p>
-  <p class="text-l mb-4 text-left tracking-wide"><a href="tel:+443300430764">Call me on +44 (0)330 0430764</a></p>
-  <p class="text-l mb-4 text-left tracking-wide"><a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9" class="link">Book a meeting (opens gCal in new tab)</a></p>
-</div>
+<Section name="feature">
+  <HeroHeader
+    class="mb-8 max-w-screen-md lg:mb-16"
+    h2Class="mb-4 lg:mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-left"
+    pClass="text-gray-500 sm:text-xl dark:text-gray-400 text-left sm:px-0 xl:px-0"
+  >
+    {#snippet h2()}Get down to brass tacks{/snippet}
+    {#snippet paragraph()}What would you like to do?{/snippet}
+  </HeroHeader>
+  <FeatureDefault>
+    <FeatureItem>
+      {#snippet icon()}<ChartPieSolid class="text-primary-600 dark:text-primary-300" />{/snippet}
+      {#snippet h3()}CV{/snippet}
+      {#snippet paragraph()}
+        <a download="ovais-oozeer-cv.pdf" href={cvpdf}>Click here to download</a>
+      {/snippet}
+    </FeatureItem>
+    <FeatureItem>
+      {#snippet icon()}<ChartPieSolid class="text-primary-600 dark:text-primary-300" />{/snippet}
+      {#snippet h3()}Services{/snippet}
+      {#snippet paragraph()}
+        <a download="services" href={cvpdf}>Click here to explore services and pricing</a>
+      {/snippet}
+    </FeatureItem>
+    <FeatureItem>
+      {#snippet icon()}<ChartPieSolid class="text-primary-600 dark:text-primary-300" />{/snippet}
+      {#snippet h3()}Email{/snippet}
+      {#snippet paragraph()}
+        Contact me at ovais@expertlypragmatic.com
+      {/snippet}
+    </FeatureItem>
+    <FeatureItem>
+      {#snippet icon()}<ChartPieSolid class="text-primary-600 dark:text-primary-300" />{/snippet}
+      {#snippet h3()}Phone{/snippet}
+      {#snippet paragraph()}Call me on +44 (0)330 0430764
+      {/snippet}
+    </FeatureItem>
+    <FeatureItem>
+      {#snippet icon()}<ChartPieSolid class="text-primary-600 dark:text-primary-300" />{/snippet}
+      {#snippet h3()}Book{/snippet}
+      {#snippet paragraph()}
+        <a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9" class="link">
+          Schedule a meeting (opens gCal in new tab)
+        </a>
+      {/snippet}
+    </FeatureItem>
+  </FeatureDefault>
+</Section>
