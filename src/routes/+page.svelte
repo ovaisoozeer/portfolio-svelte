51b66cc7ba5f87e8f0cb8f0e634efe557d06fe6d
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-11 16:25:42 +0000

    Updates home page to use flowbite components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,39 @@
-<div class="mb-24 flex w-full flex-col items-center">
-  <h1 class="m-4 text-center text-6xl tracking-wide">Hi, my name is Ovais</h1>
-  <h3 class="text-l mb-8 text-center tracking-wide"><em>(əʊveɪs, he/him)</em></h3>
-  <p class="text-l mb-4 text-center tracking-wide">
-    A platform-agnostic, hands-on tech lead who enjoys collaboration.
-  </p>
-  <p class="text-l mb-4 text-center tracking-wide">
-    I build applications, services, and platforms for the public and private sector.
-  </p>
-</div>
-<div class="mt-8 mb-24 grid grid-cols-1 gap-6 md:grid-cols-3">
-  <a href="about">
-    <div class="card bg-base-200 shadow-xl p-8">
-      <div class="card-body">
-        <h2 class="card-title justify-center text-center tracking-wide">About</h2>
-      </div>
-    </div>
-  </a>
-  <a href="explore">
-    <div class="card bg-base-200 shadow-xl p-8">
-      <div class="card-body">
-        <h2 class="card-title justify-center text-center tracking-wide">
-          Content, Community, and Events
-        </h2>
-      </div>
-    </div>
-  </a>
-  <a href="brass-tacks">
-    <div class="card bg-base-200 shadow-xl p-8">
-      <div class="card-body">
-        <h2 class="card-title justify-center text-center tracking-wide">Brass tacks</h2>
-      </div>
-    </div>
-  </a>
-</div>
+<script lang="ts">
+  import { Section, HeroHeader } from 'flowbite-svelte-blocks';
+  import { Button } from 'flowbite-svelte';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Section name="heroVisual">
+  <div class="mr-auto place-self-center lg:col-span-7">
+    <HeroHeader
+      h1Class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
+      pClass="text-left max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 sm:px-0 lg:px-0 xl:px-0"
+    >
+      {#snippet h1()}Hi, my name is Ovais{/snippet}
+      {#snippet paragraph()}
+        (əʊveɪs, he/him)<br />
+        A platform-agnostic, hands-on tech lead who enjoys collaboration.<br />
+        I build applications, services, and platforms for the public and private sector.
+      {/snippet}
+      <a href="brass-tacks">
+        <Button size="xl" color="red" class="mr-3 inline-flex items-center justify-center">
+          Get started<ArrowRightOutline size="md" class="-mr-1 ml-2" />
+        </Button>
+      </a>
+      <a href="about">
+        <Button color="light" size="xl" class="mr-3 inline-flex items-center justify-center">
+          About
+        </Button>
+      </a>
+      <a href="explore">
+        <Button color="light" size="xl" class="inline-flex items-center justify-center">
+          Explore
+        </Button>
+      </a>
+    </HeroHeader>
+  </div>
+  <!-- <div class="hidden lg:col-span-5 lg:mt-0 lg:flex">
+    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+  </div> -->
+</Section>
