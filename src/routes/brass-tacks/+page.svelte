35a5171034889ff2aebcbb57cd828ec0bce491ee
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-12 00:42:23 +0000

    Updates brass tacks

--- src/routes/brass-tacks/+page.svelte
+++ src/routes/brass-tacks/+page.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
   import cvpdf from '$lib/assets/cv.pdf';
   import { Section, HeroHeader, FeatureDefault, FeatureItem } from 'flowbite-svelte-blocks';
-  import {
-    CalendarPlusOutline,
-    DownloadOutline,
-    MailBoxOutline,
-    PersonChalkboardOutline,
-    PhoneOutline
-  } from 'flowbite-svelte-icons';
+  import { CalendarPlusOutline, DownloadOutline, MailBoxOutline } from 'flowbite-svelte-icons';
 </script>
 
 <Section name="feature">
@@ -17,47 +11,33 @@
     pClass="text-gray-500 sm:text-xl dark:text-gray-400 text-left sm:px-0 xl:px-0"
   >
     {#snippet h2()}Get down to brass tacks{/snippet}
-    {#snippet paragraph()}What would you like to do?{/snippet}
+    {#snippet paragraph()}{/snippet}
   </HeroHeader>
   <FeatureDefault>
     <FeatureItem>
       {#snippet icon()}<DownloadOutline class="text-primary-600 dark:text-primary-300" />{/snippet}
-      {#snippet h3()}CV{/snippet}
+      {#snippet h3()}Download CV{/snippet}
       {#snippet paragraph()}
-        <a download="ovais-oozeer-cv.pdf" href={cvpdf}>Click here to download</a>
-      {/snippet}
-    </FeatureItem>
-    <FeatureItem>
-      {#snippet icon()}<PersonChalkboardOutline
-          class="text-primary-600 dark:text-primary-300"
-        />{/snippet}
-      {#snippet h3()}Services{/snippet}
-      {#snippet paragraph()}
-        <a download="services" href={cvpdf}>Click here to explore services and pricing</a>
+        Click <a download="ovais-oozeer-cv.pdf" href={cvpdf}><u>here</u> </a> to download a PDF
       {/snippet}
     </FeatureItem>
     <FeatureItem>
       {#snippet icon()}<MailBoxOutline class="text-primary-600 dark:text-primary-300" />{/snippet}
-      {#snippet h3()}Email{/snippet}
+      {#snippet h3()}Say Hello{/snippet}
       {#snippet paragraph()}
-        Contact me at ovais@expertlypragmatic.com
-      {/snippet}
-    </FeatureItem>
-    <FeatureItem>
-      {#snippet icon()}<PhoneOutline class="text-primary-600 dark:text-primary-300" />{/snippet}
-      {#snippet h3()}Phone{/snippet}
-      {#snippet paragraph()}Call me on +44 (0)330 0430764
+        Click <a href="mailto:ovais@expertlypragmatic.com"><u>here</u></a> to email ovais@expertlypragmatic.com, or<br />
+        <a href="tel:+443300430764"><u>Call</u></a> 0330 0430764
       {/snippet}
     </FeatureItem>
     <FeatureItem>
       {#snippet icon()}<CalendarPlusOutline
           class="text-primary-600 dark:text-primary-300"
         />{/snippet}
-      {#snippet h3()}Book{/snippet}
+      {#snippet h3()}No Strings Attached{/snippet}
       {#snippet paragraph()}
-        <a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9" class="link">
-          Schedule a meeting (opens gCal in new tab)
-        </a>
+        Click <a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9">
+          <u>here</u>
+        </a> to book an intro call
       {/snippet}
     </FeatureItem>
   </FeatureDefault>
