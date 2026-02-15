7381fc93c10c268972c17aeacee064ef0483c5e9
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-15 21:49:46 +0000

    Removes blog page. Will continue to use blogger for the time being

--- src/routes/blog/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="mb-8 flex w-full flex-col">
-  <h2 class="m-4 text-center text-4xl tracking-wide">Blog</h2>
-  <p class="text-l mb-4 text-left tracking-wide hidden">
-    TODO: Dynamic page for blog content.
-  </p>
-</div>
