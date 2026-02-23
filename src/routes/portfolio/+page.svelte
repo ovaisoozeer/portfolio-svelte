37163a0dd19f1c72c02d2cdad39065c5524a29b9
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-23 02:05:48 +0000

    Updates portfolio to use content rather than FAQ blocks

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,117 +1,84 @@
 <script lang="ts">
-  import { Section, Faq, FaqItem } from 'flowbite-svelte-blocks';
+  import { Section, Faq, FaqItem, ContentWithImage, Content } from 'flowbite-svelte-blocks';
+  import dluhc from '$lib/assets/dluhc.png';
+  import mars from '$lib/assets/mars.png';
+  import tvg from '$lib/assets/tvg.jpg';
 </script>
 
-<Section name="portfolio">
-  <h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-    Cabinet Office - Voter card registration
-  </h2>
-  <Faq>
-    <div>
-      <FaqItem>
-        {#snippet h3()}Role{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">Architecture and Prototyping</p>
-      </FaqItem>
-      <FaqItem>
-        {#snippet h3()}Task{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">
-          National infrastructure project architecture and service assessment
-        </p>
-      </FaqItem>
-    </div>
-    <div>
-      <FaqItem>
-        {#snippet h3()}Description{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">
-          As an architect on the voter cards programme, I had a hand in designing the architecture
-          of the system, working closely with specialists on threat modelling and user experience. I
-          owned the technical beta assessment for the overseas and absent voter registration route
-          of the programme. I also developed the GDS design system prototype, re-using components of
-          DVLA, Blue Badge, and HMPO sites.
-        </p>
-      </FaqItem>
-    </div>
-  </Faq>
-  <h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-    Mars, Inc.
-  </h2>
-  <Faq>
-    <div>
-      <FaqItem>
-        {#snippet h3()}Role{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">Tech Lead</p>
-      </FaqItem>
-      <FaqItem>
-        {#snippet h3()}Task{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">
-          Set up a platform engineering team, delivering coaching and tooling as needed within a
-          hierarchical business
-        </p>
-      </FaqItem>
-    </div>
-    <div>
-      <FaqItem>
-        {#snippet h3()}Description{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">
-          Coming in as Tech Lead, I helped the team to increase predictability and productiveness.
-          This, through coaching of the business and tech stakeholders to have a more
-          product-aligned mindset, supporting a culture of continuous improvement, and developing
-          tooling to support business users and feature teams. The ways of working surmised in the
-          book Team Topologies are very much in line with what I advocated here.
-        </p>
-      </FaqItem>
-    </div>
-  </Faq>
-  <h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-    The Very Group
-  </h2>
-  <Faq>
-    <div>
-      <FaqItem>
-        {#snippet h3()}Role{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">Engineering Manager (Conversational UI)</p>
-      </FaqItem>
-      <FaqItem>
-        {#snippet h3()}Task{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">
-          Reporting to the CTO, coaching an internal team, and overseeing architectural changes
-        </p>
-      </FaqItem>
-    </div>
-    <div>
-      <FaqItem>
-        {#snippet h3()}Description{/snippet}
-        <p class="text-gray-500 dark:text-gray-400">
-          I was asked to manage an internal team at TVG, during a period of major refactoring and
-          unexpected changes of staff. I supported a diverse team with their personal development
-          whilst ensuring that the product owner's vision was delivered.
-        </p>
-      </FaqItem>
-    </div>
-  </Faq>
+<Section name="contentwithimg">
+  <ContentWithImage>
+    {#snippet h2()}Cabinet Office - Architect - Voter card registration{/snippet}
+    <p class="mb-4">National infrastructure project architecture and service assessment</p>
+    <p class="mb-4">
+      As an architect on the voter cards programme, I had a hand in designing the architecture of
+      the system, working closely with specialists on threat modelling and user experience. I owned
+      the technical beta assessment for the overseas and absent voter registration route of the
+      programme. I also developed the GDS design system prototype, re-using components of DVLA, Blue
+      Badge, and HMPO sites.
+    </p>
 
-  <h1 class="text-center">Selected other projects and engagements:</h1>
-  <ul class="mb-16 w-full text-center">
-    <li class="list-row mb-4">
+    {#snippet image()}
+      <div class="bg-white">
+        <img class="w-full rounded-lg" src={dluhc} alt="Cabinet office logo" />
+      </div>
+    {/snippet}
+  </ContentWithImage>
+  <ContentWithImage>
+    {#snippet h2()}Mars, Inc. - Tech Lead - Platform engineering{/snippet}
+    <p class="mb-4">National infrastructure project architecture and service assessment</p>
+    <p class="mb-4">
+      I was tasked to set up a platform engineering team, delivering coaching and tooling as needed
+      within a hierarchical business unit. I helped the team to increase predictability and
+      productiveness through coaching of business and tech stakeholders in a more product-aligned
+      mindset, supporting a culture of continuous improvement, and developing tooling to support
+      business users and feature teams.
+    </p>
+    {#snippet image()}
+      <div class="bg-white">
+        <img class="w-full rounded-lg" src={mars} alt="Cabinet office logo" />
+      </div>
+    {/snippet}
+  </ContentWithImage>
+  <ContentWithImage>
+    {#snippet h2()}The Very Group - Engineering Manager - Conversational UI team{/snippet}
+    <p class="mb-4">National infrastructure project architecture and service assessment</p>
+    <p class="mb-4">
+      I was asked to manage an internal team at TVG, during a period of major refactoring and
+      unexpected changes of staff. I supported a diverse team with their personal development whilst
+      ensuring that the product owner's vision was delivered.
+    </p>
+    {#snippet image()}
+      <div class="bg-white">
+        <img class="w-full rounded-lg" src={tvg} alt="Cabinet office logo" />
+      </div>
+    {/snippet}
+  </ContentWithImage>
+</Section>
+<Section name="content">
+  <Content>
+    {#snippet h2()}Selected other projects and engagements{/snippet}
+
+    <p class="mb-4 font-light"></p>
+    <p class="mb-4 font-light">
       ✈️ Civil Aviation Authority - Obstruction Management website - Developer
-    </li>
-    <li class="list-row mb-4">
+    </p>
+    <p class="mb-4 font-light">
       🚉 Rail Delivery Group - National Rail Enquiries, Common Services - Architect
-    </li>
-    <li class="list-row mb-4">💹 Schroders Personal Wealth - Lead capture website - Tech Lead</li>
-    <li class="list-row mb-4">📊 Euromonitor International - ETL Platform - Developer</li>
-    <li class="list-row mb-4">⚕️ BUPA - Test automation framework - Developer</li>
-    <li class="list-row mb-4">🚢 Rolls Royce Marine - Ship-to-shore data platform - Tech Lead</li>
-    <li class="list-row mb-4">
+    </p>
+    <p class="mb-4 font-light">💹 Schroders Personal Wealth - Lead capture website - Tech Lead</p>
+    <p class="mb-4 font-light">📊 Euromonitor International - ETL Platform - Developer</p>
+    <p class="mb-4 font-light">⚕️ BUPA - Test automation framework - Developer</p>
+    <p class="mb-4 font-light">🚢 Rolls Royce Marine - Ship-to-shore data platform - Tech Lead</p>
+    <p class="mb-4 font-light">
       🚗 Toyota Financial Services - B2C private leasing platform - Tech Lead
-    </li>
-    <li class="list-row mb-4">
+    </p>
+    <p class="mb-4 font-light">
       🏥 NHS Business Services Authority - Patient preferences service - Developer
-    </li>
-    <li class="list-row mb-4">
+    </p>
+    <p class="mb-4 font-light">
       🏫 Skills Funding Agency - National Apprenticeship programme - Developer
-    </li>
-    <li class="list-row mb-4">🏨 DeVere - Venues Website - Developer</li>
-    <li class="list-row mb-4">🎼 ABRSM - Aural trainer app - Developer</li>
-  </ul>
+    </p>
+    <p class="mb-4 font-light">🏨 DeVere - Venues Website - Developer</p>
+    <p class="mb-4 font-light">🎼 ABRSM - Aural trainer app - Developer</p>
+  </Content>
 </Section>
