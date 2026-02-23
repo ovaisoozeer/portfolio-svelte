97b19089eb3212bfe27c6fde91f9ff8fc2ca7a96
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-23 11:05:19 +0000

    Adds GH link

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -18,13 +18,20 @@
   <Cta ctatype="heading" class="mt-16">
     {#snippet h2()}Working in the open{/snippet}
     <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-      I prefer to speak candidly.
+      I prefer candour to confidentiality.
     </p>
     <a
       href="services"
       class="mr-2 mb-2 rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 focus:outline-none dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
     >
       Check out my pricing
+    </a>
+    <a
+      target="_blank"
+      href="https://github.com/ovaisoozeer"
+      class="mr-2 mb-2 rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 focus:outline-none dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    >
+      GitHub
     </a>
   </Cta>
   <Cta ctatype="heading" class="mt-16">
