276d15a66b9312129769a49195c2c11f68cf25cd
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2026-02-23 01:35:17 +0000

    Updates contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,34 +10,35 @@
     h2Class="mb-4 lg:mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-left"
     pClass="text-gray-500 sm:text-xl dark:text-gray-400 text-left sm:px-0 xl:px-0"
   >
-    {#snippet h2()}Get down to brass tacks{/snippet}
-    {#snippet paragraph()}{/snippet}
+    {#snippet h2()}Available actions{/snippet}
+    {#snippet paragraph()}Get straight down to brass tacks{/snippet}
   </HeroHeader>
   <FeatureDefault>
     <FeatureItem>
       {#snippet icon()}<InboxOutline class="text-primary-600 dark:text-primary-300" />{/snippet}
-      {#snippet h3()}Say Hello 👋🏽{/snippet}
+      {#snippet h3()}Contact{/snippet}
       {#snippet paragraph()}
-        <a href="mailto:ovais@expertlypragmatic.com"><u>Email</u></a> ovais@expertlypragmatic.com<br />
-        <a href="tel:+443300430764"><u>Call</u></a> 0330 0430764
+        <a href="mailto:ovais@expertlypragmatic.com"><u>Email</u></a> ovais@expertlypragmatic.com<br
+        />
+        <a href="tel:+443300430764"><u>Phone</u></a> 0330 0430764
       {/snippet}
     </FeatureItem>
     <FeatureItem>
       {#snippet icon()}<DownloadOutline class="text-primary-600 dark:text-primary-300" />{/snippet}
-      {#snippet h3()}Download my CV{/snippet}
+      {#snippet h3()}CV{/snippet}
       {#snippet paragraph()}
-        Click <a download="ovais-oozeer-cv.pdf" href={cvpdf}><u>here</u> </a> for a PDF
+        <a download="ovais-oozeer-cv.pdf" href={cvpdf}><u>Download PDF</u> </a>
       {/snippet}
     </FeatureItem>
     <FeatureItem>
       {#snippet icon()}<CalendarPlusOutline
           class="text-primary-600 dark:text-primary-300"
         />{/snippet}
-      {#snippet h3()}No Strings Attached{/snippet}
+      {#snippet h3()}Calendar{/snippet}
       {#snippet paragraph()}
-        Click <a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9">
-          <u>here</u>
-        </a> to book an intro call
+        <a target="_blank" href="https://calendar.app.google/PteCMAz5AeiBRbrp9">
+          <u>Book meeting</u>
+        </a>
       {/snippet}
     </FeatureItem>
   </FeatureDefault>
